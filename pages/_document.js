d4f2f8e518574fe741d96b4e55ea09ca54fb04cd
Leroy Salih <Leroy.Salih@gmail.com>
2020-11-11 08:30:07 +0300

    Adding Verification

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <meta name="google-site-verification" content="QvYp7LGBDePMzWpyH6Z0CBtn_hwp23B1vgy9CKGOZuI" />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet"/>
         </Head>
         <body>
