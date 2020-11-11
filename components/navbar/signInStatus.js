caa93904cbf0d8e2d5f3e81d5bd054d33d8e4ba8
Leroy Salih <Leroy.Salih@gmail.com>
2020-11-11 13:45:40 +0300

    Working on Langing Page

--- components/navbar/signInStatus.js
+++ components/navbar/signInStatus.js
@@ -1,11 +1,26 @@
-import {useSession} from 'next-auth/client';
+import useSWR from 'swr';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 const SignInStatus = () => {
-  const [session, loading] = useSession();
+  const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+  const {data, error} = useSWR('/api/auth/session', fetcher);
+
+  const [ session, loading ] = useSession()
 
   return (<>
     {!session && "Sign In"}
     {session && session.user.name}
+
+    {!session && <>
+      Not signed in <br/>
+      <button onClick={signIn}>Sign in</button>
+    </>}
+
+    {session && <>
+      Signed in as {session.user.name}, {session.id} <br/>
+      <button onClick={signOut}>Sign out</button>
+    </>}
     </>)
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from '../components/layout';
 import useSWR from 'swr';
 import Link from 'next/link';
-
+import Image from 'next/image';
 import { signIn, signOut, useSession } from 'next-auth/client'
 // import fs from 'fs';
 
@@ -17,22 +17,51 @@ const IndexPage = ({dataProp, courses}) => {
   if (error) return <div>failed to load</div>
 
   return (<>
-    <div>hello </div>
-     {courses.map((path, index) => <Link key={index} href={`/${path}`}><a>{path}</a></Link>)}
-    {!session && <>
-      Not signed in <br/>
-      <button onClick={signIn}>Sign in</button>
-    </>}
-
-    {session && <>
-      Signed in as {session.user.name}, {session.id} <br/>
-      <button onClick={signOut}>Sign out</button>
-    </>}
-
+    
     {session && <>
     <div>{session.user.uid}</div>
     <div>{session.user.provider}</div>
     </>}
+
+    <div className="pageLayout">
+      <div className="pageText">
+        <h1>Computer Science</h1>
+        <h2>Key Points</h2>
+        <div>
+
+          A collection of resources to help you revise the subject and maximise your grades. 
+        </div>
+      </div>
+      <div className="pageHero">
+        <Image alt={"Pupils Studying"}
+          width={800}
+          height={640}
+          src="/images/home-page-banner.png"/>
+      </div>
+        
+
+    </div>
+
+    <style jsx>{`
+
+    h1 {
+      color: rgb(0, 206, 209)
+    }
+    
+    .pageLayout {
+      display: grid;
+      
+      grid-template-columns: auto 640px;
+    }
+
+    .pageText {
+      display: flex;
+      flex-direction: column;
+      justify-items: center;
+    }
+    
+    `}</style>
+    
     </>
     )
 
